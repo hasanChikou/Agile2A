c24203c6892eb218d28e3d36878200fc3f456de7
Nemesis <77245916+Nizalk@users.noreply.github.com>
2022-05-22 23:46:08 +0100

    Backend

---
+++ BACK_END/src/test/java/com/adoption/adoptionanimal/AdoptionAnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoption.adoptionanimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionAnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
